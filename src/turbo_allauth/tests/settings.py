a2b08d84584ae34f0d4fb64beb75954a296d1c72
Dan Jacob <danjac2018@gmail.com>
2021-01-02 14:36:27 +0200

    Signup tests

--- src/turbo_allauth/tests/settings.py
+++ src/turbo_allauth/tests/settings.py
@@ -1,7 +1,7 @@
 # Standard Library
 import pathlib
 
-SECRET_KEY = "o7pb3_ibyl@3%jhc85qj*dgh5etxmzxpg89%$85$#v92&8e0ub"
+SECRET_KEY = "seekret"
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "mem_db"},
 }
--- src/turbo_allauth/tests/test_views.py
+++ src/turbo_allauth/tests/test_views.py
@@ -21,7 +21,6 @@ class TestLogin:
         )
 
         assert resp.status_code == 200
-
         assert b'target="login-form"' in resp.content
         assertTemplateUsed(resp, "account/_login.html")
 
@@ -32,3 +31,40 @@ class TestLogin:
         )
         print(resp.content)
         assert resp.url == "/"
+
+
+class TestSignup:
+    def test_get(self, client):
+        resp = client.get(reverse("account_signup"))
+        assert resp.status_code == 200
+        assert b'id="signup-form"' in resp.content
+        assertTemplateUsed(resp, "account/_signup.html")
+
+    def test_post_unsuccessful(self, client):
+        resp = client.post(
+            reverse("account_signup"),
+            {
+                "username": "tester",
+                "email": "bad-email",
+                "password1": "bad-pass",
+                "password2": "bad-pass-match",
+            },
+        )
+        assert resp.status_code == 200
+        assert b'target="signup-form"' in resp.content
+        assertTemplateUsed(resp, "account/_signup.html")
+
+    def test_post_successful(self, client, user_model):
+        resp = client.post(
+            reverse("account_signup"),
+            {
+                "username": "tester",
+                "email": "tester@gmail.com",
+                "password1": "good-pass-1234",
+                "password2": "good-pass-1234",
+            },
+        )
+
+        assert resp.url == "/"
+        user = user_model.objects.get(username="tester")
+        assert user.check_password("good-pass-1234")
