7cd5fa35f576c34aeb0c6de433e1d3a0cdfbdbe6
Dan Jacob <danjac2018@gmail.com>
2021-01-02 12:36:59 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Third Party Libraries
+from setuptools import setup
+
+version = "0.0.1"
+
+setup(
+    name="django-turbo-allauth",
+    version=version,
+    author="Dan Jacob",
+    author_email="danjac2018@gmail.com",
+    url="https://github.com/hotwire-django/django-turbo-allauth",
+    description="Hotwired/Turbo subclasses for allauth views",
+    long_description=open("README.md").read() + "\n\n" + open("CHANGES.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.8",
+    requires=["django (>=3.1)", "django-turbo-response", "django-allauth"],
+    packages=["turbo_allauth"],
+    package_dir={"": "src"},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
