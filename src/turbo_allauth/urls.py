7d241d658bb623fdb9ac8663bef12dbec931cf0a
Dan Jacob <danjac2018@gmail.com>
2021-01-02 12:42:54 +0200

    URL and views

---
+++ src/turbo_allauth/urls.py
@@ -0,0 +1,36 @@
+from django.urls import path, include, re_path
+from .views import account, socialaccount
+
+urlpatterns = [
+    path(
+        "password/change/",
+        account.password_change,
+        name="account_change_password",
+    ),
+    path(
+        "password/set/",
+        account.password_set,
+        name="account_set_password",
+    ),
+    path(
+        "password/reset/",
+        account.password_reset,
+        name="account_reset_password",
+    ),
+    re_path(
+        r"^account/password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        account.password_reset_from_key,
+        name="account_reset_password_from_key",
+    ),
+    path(
+        "email/",
+        account.email,
+        name="account_email",
+    ),
+    path(
+        "social/signup/",
+        socialaccount.signup,
+        name="socialaccount_signup",
+    ),
+    path("", include("allauth.urls")),
+]
---
+++ src/turbo_allauth/views/socialaccount.py
@@ -0,0 +1,10 @@
+# Third Party Libraries
+from allauth.socialaccount import views as socialaccount_views
+from turbo_response.mixins import TurboStreamFormMixin
+
+
+class SignupView(TurboStreamFormMixin, socialaccount_views.SignupView):
+    turbo_stream_target = "signup-form"
+
+
+signup = SignupView.as_view()
