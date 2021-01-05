5bbeb261a9332ae42b97b1053364341fe83a595d
Dan Jacob <danjac2018@gmail.com>
2021-01-05 14:33:13 +0200

    Changes updated

---
+++ CHANGES.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## [0.0.2] - 2021-5-1
+
+Update to latest *django-turbo-response*, remove template partials
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # Third Party Libraries
 from setuptools import find_packages, setup
 
-version = "0.0.1"
+version = "0.0.2"
 
 setup(
     name="django-turbo-allauth",
